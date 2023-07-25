05e105056565920d00619537eb706a2988738d85
badbuck69 <6sgencer9@gmail.com>
2023-07-25 18:35:37 +0300

    Açıklamalar eklendi.

--- odev2.py
+++ odev2.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template,request
 import requests
-app = Flask(__name__)
 
+#uygulama oluşturma
+app = Flask(__name__)
+#Ana Sayfa
 @app.route("/")
 def index():
     return render_template("index.html")
-
+# /pokemon adresine gönderilen get ve post işlemleri
 @app.route("/pokemon", methods=['GET',"POST"])
 def pokemon():
     if request.method == 'POST':
@@ -14,17 +16,19 @@ def pokemon():
 
         if pokename == '':
             return "Bad request", 400
-
+        #Pokeapi den başarılı yanıt alınırsa
         elif result.status_code == 200:
             json_data = result.json()
+            #pokemon.html şablonunu renderlayarak yanıt döndürülür
             return render_template('pokemon.html', result = json_data)
         elif result.status_code == 404:
             return "Not Found", 404
 
         else:
             return render_template('pokemon.html', result="Pokemon not found")
+    #Eğer Get isteği gelirse
     else:        
         return render_template('pokemon.html') 
-
+#Flask çalıştırılması
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
